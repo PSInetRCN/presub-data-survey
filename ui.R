21525e1997d826316e0273f740d7107dbd84320a
Jessica Guo <jessicaguo@email.arizona.edu>
2023-10-03 09:07:41 -0700

    Added note about the required fields and submit button

--- ui.R
+++ ui.R
@@ -3,6 +3,6 @@ fluidPage(
   theme = bs_theme(version = 3),
   surveyOutput(df = df,
                survey_title = "Pre-submission survey",
-               survey_description = h4(HTML("Thank you for your interest in contributing to PSInet. Please complete the following form <b>for a single dataset</b> and we will contact you with further submission details.")),
+               survey_description = h4(HTML("Thank you for your interest in contributing to PSInet. Please complete the following form <b>for a single dataset</b>. <br> <br> <i>Note that all required fields must be completed to activate the 'Submit' button, including each row of the meteorological matrix. </i>")),
                theme = "#358DBD")
 )
